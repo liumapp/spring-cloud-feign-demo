4486a28724f31e3ec272a322463ba3ccb37b4799
liumapp <liumapp.com@gmail.com>
2018-01-29 11:24:57 +0800

    add pom dependency

--- feign-demo-api/src/main/java/com/liumapp/demo/feign/api/service/HelloService.java
+++ feign-demo-api/src/main/java/com/liumapp/demo/feign/api/service/HelloService.java
@@ -1,6 +1,10 @@
 package com.liumapp.demo.feign.api.service;
 
+import com.liumapp.demo.feign.api.entity.User;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by liumapp on 1/29/18.
@@ -10,6 +14,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/refactor")
 public interface HelloService {
 
-    
+    @RequestMapping("/hello1")
+    String hello (@RequestParam("name") String name);
+
+    @RequestMapping("/hello2")
+    String hello (@RequestHeader("name") String name , @RequestHeader("age") Integer age);
+
+    @RequestMapping("/hello3")
+    String hello (@RequestBody User user);
 
 }
