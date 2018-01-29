7c3cd21ee7d17283bc9fcb9ae138f09445658279
liumapp <liumapp.com@gmail.com>
2018-01-29 11:21:54 +0800

    add api servcie

---
+++ feign-demo-api/src/main/java/com/liumapp/demo/feign/api/entity/User.java
@@ -0,0 +1,45 @@
+package com.liumapp.demo.feign.api.entity;
+
+/**
+ * Created by liumapp on 1/29/18.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+public class User {
+
+    private String name;
+
+    private Integer age;
+
+    public User() {
+    }
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
---
+++ feign-demo-api/src/main/java/com/liumapp/demo/feign/api/service/HelloService.java
@@ -0,0 +1,15 @@
+package com.liumapp.demo.feign.api.service;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by liumapp on 1/29/18.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+@RequestMapping("/refactor")
+public interface HelloService {
+
+    
+
+}
